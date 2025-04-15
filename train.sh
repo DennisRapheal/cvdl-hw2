039bdb36f691f8ce1178e6fd3be1260df340a551
jzwang <a15923647@gmail.com>
2025-04-15 17:43:55 +0000

    mix train complete

--- train.sh
+++ train.sh
@@ -6,3 +6,5 @@
 
 python train.py --model_name resnext50_32x4d_hard_transform --model_type resnext50_32x4d --from_pretrained ./checkpoints/resnext50_32x4d.pth --batch_size 32 --num_epochs 30
 python train.py --model_name resnext50_32x4d_hard_transform_resize256 --model_type resnext50_32x4d --from_pretrained ./checkpoints/resnext50_32x4d.pth --batch_size 16 --num_epochs 25
+
+python train.py --model_name resnext101_32x8d_hard_transform_resize256 --model_type resnext101_32x8d --batch_size 16 --num_epochs 35
